b50e1d794fc3da81f569ff6b30c6a8f6e468ff52
revive <romain.blazevic@gmail.com>
2022-01-31 15:03:22 +0100

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,7 @@
 #include "cpu.hpp"
 
 int main(int argc, char* argv[]) {
-    CPU * cpu = new CPU("../res/test_opcode.ch8");
-
-//    std::vector<std::uint8_t> rom = cpu->readROM("../res/Pong.ch8");
-
-//    for(int i = 0x200; i < rom.size() + 0x200; i+=2) {
-//        std::cout << "code : " << std::hex << cpu->fetchCode(i) << '\n';
-//    }
+    auto cpu = std::make_unique<CPU>("../res/test_opcode.ch8");
     cpu->run();
 
     return 0;
